217e2b670488d3ec3ad073318213f75670300df9
Chirag goyal <chirag.goyal@uneecops.in>
2025-05-01 15:58:49 +0530

    'sd'

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 10000;
 const upload = multer({ dest: 'uploads/' });
 
 app.get('/', (req, res) => {
-  res.send('Welcome to File Upload Middleware API! Use POST /upload to upload files.');
+  res.send(' Welcome to File Upload Middleware API! Use POST /upload to upload files.');
 });
 
 app.post('/upload', upload.single('file'), (req, res) => {
